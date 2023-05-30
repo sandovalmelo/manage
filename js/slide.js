b4c8ce0281c2adb02908423112099ee9b3653f01
sandovalmelo <juniormelo777@gmail.com>
2023-05-30 20:06:02 -0300

    add move slides scripts

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,52 @@ export default class Slide {
 	constructor(slide, container) {
 		this.slide = document.getElementById(slide);
 		this.container = document.getElementById(container);
+		this.dist = {
+			finalPosition: 0,
+			startX: 0,
+			movement: 0
+		};
+	}
+
+	moveSlide(distX) {
+		this.dist.movePosition = distX;
+		this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+	}
+
+	updatePosition(clientX) {
+		this.dist.movement = (this.dist.startX - clientX) * 1.6;
+		return this.dist.finalPosition - this.dist.movement;
 	}
 
 	onStart(event) {
 		event.preventDefault();
-		console.log(event);
+		this.dist.startX = event.clientX;
+		this.container.addEventListener("mousemove", this.onMove);
+	}
+
+	onMove(event) {
+		const finalPosition = this.updatePosition(event.clientX);
+		this.moveSlide(finalPosition);
+	}
+
+	onEnd(event) {
+		this.container.removeEventListener("mousemove", this.onMove);
+		this.dist.finalPosition = this.dist.movePosition;
 	}
 
 	addSlideEvents() {
 		this.container.addEventListener("mousedown", this.onStart);
+		this.container.addEventListener("mouseup", this.onEnd);
+	}
+
+	bindEvents() {
+		this.onStart = this.onStart.bind(this);
+		this.onMove = this.onMove.bind(this);
+		this.onEnd = this.onEnd.bind(this);
 	}
 
 	init() {
+		this.bindEvents();
 		this.addSlideEvents();
 		return;
 	}
